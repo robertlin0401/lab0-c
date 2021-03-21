d8839178b32365b1b8b76e1ac2a8806f1fd0930d
robertlin0401 <robertlin0401@gmail.com>
2021-03-22 01:31:56 +0800

    Revise: implement q_reverse
    Directly return if the queue is NULL or empty.
    If not, set head as new tail, then iterate through the queue from
    the next element of head. For each element, move it to where
    before head and set it as new head.

--- queue.c
+++ queue.c
@@ -141,8 +141,14 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (q == NULL || q->head == NULL) return;
+    q->tail = q->head;
+    while (q->tail->next != NULL) {
+        list_ele_t *target = q->tail->next;
+        q->tail->next = target->next;
+        target->next = q->head;
+        q->head = target;
+    }
 }
 
 /*
